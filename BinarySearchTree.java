a08a042c364076332b4ef3d34ed526bc0257268c
Axel Pontén <46627178+axelponten@users.noreply.github.com>
2019-05-10 13:07:18 -0500

    Add files via upload

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,4 +1,4 @@
-
+// AUTHOR: AXEL PONTEN
 public class BinarySearchTree {
 	
 	private BinarySearchTreeNode root;
@@ -9,6 +9,42 @@ public class BinarySearchTree {
 	
 	public BinarySearchTree(BinarySearchTreeNode n) {
 		root = n;
+	}
+	
+	public int hashCode(String s) {
+		
+	}
+	
+	public void insert(String s) {
+		insert(new BinarySearchTreeNode(s));
+	}
+	
+	public void insert(BinarySearchTreeNode n) {
+		
+	}
+	
+	public Node search(String s) {
+		
+	}
+	
+	public Node findParent(String s) {
+		
+	}
+	
+	public void delete(String s) {
+		
+	}
+	
+	public void print_Preorder() {
+		
+	}
+	
+	public void print_Inorder() {
+		
+	}
+	
+	public void print_Postorder() {
+		
 	}
 	
 	// SETTERS AND GETTERS
